e198d004ec5f746b74643f2ff7a23daf4bf8251f
tumuruub0108 <tumuruub0108@mail.com>
2025-09-05 21:35:55 +0900

    fixed some codes

--- california.py
+++ california.py
@@ -2,7 +2,11 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from pathlib import Path
 
-df_house = pd.read_csv("../example/datatasets/housing.csv")
+# read and show the dataset
+BASE_DIR = Path(__file__).resolve().parent
+file_path = BASE_DIR /"datasets"/"housing.csv"
+df_house = pd.read_csv(file_path)
 
 print(df_house.head(5))
