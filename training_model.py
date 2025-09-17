d6ea722272db6c0e67df530319ed3bb904165a44
tumuruub0108 <Tumuruub0108@gmail.com>
2025-09-17 18:20:34 +0900

    added some things

--- training_model.py
+++ training_model.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.preprocessing import add_dummy_feature
+import matplotlib.pyplot as plt
 
 
 np.random.seed(42) # to make this code example reproducible
@@ -10,7 +11,7 @@ y = 4 + 3 * X + np.random.randn(m, 1) # column vector
 X_b = add_dummy_feature(X) # add x0 = 1 to each instance
 theta_best = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
 
-print(theta_best)
+print("=>", theta_best)
 
 X_new = np.array([[0]])
 
@@ -18,4 +19,9 @@ X_new = np.array([[0]])
 X_new = np.array([[0], [2]])
 X_new_b = add_dummy_feature(X_new) 
 y_predict = X_new_b @ theta_best
-print(y_predict)
+print("==>",y_predict)
+
+
+plt.plot(X_new, y_predict, "r-", label="Predictions")
+plt.plot(X, y, "b.")
+plt.show()
